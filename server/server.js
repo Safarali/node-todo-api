ea62faee2127ffc291b7241eaf13b8dcaf12ea0f
Safarali <mr.safarali@gmail.com>
2017-11-20 21:57:42 -0800

    Return 404 instead 400

--- server/server.js
+++ server/server.js
@@ -31,16 +31,16 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
     if(!ObjectID.isValid(id)){
-        return res.status(400).send();
+        return res.status(404).send();
     }
 
     Todo.findById(id).then(todo => {
         if(!todo){
-            return res.status(400).send();
+            return res.status(404).send();
         }
         res.send({todo})
     }).catch((e) => {
-        res.status(400).send();
+        res.status(404).send();
     });
 });
 
